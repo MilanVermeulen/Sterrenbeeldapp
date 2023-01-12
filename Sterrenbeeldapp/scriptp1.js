57b0bb92bf3be6724cc65fba60a81c3b0c6abb88
Alexander Goyens <alexander.goyens@gmail.com>
2023-01-12 19:50:23 +0100

    Fixed zodiac IF-statements

--- Sterrenbeeldapp/scriptp1.js
+++ Sterrenbeeldapp/scriptp1.js
@@ -13,90 +13,82 @@ fetch("https://cataas.com/cat?json=true").then(
   document.getElementById("catImg").src= catURL + data.url;
 })
 
-function getZodiac(){
-    //GET DATE
-    var birthyear = sessionStorage.getItem('year')
-    var dateOfBirth = new Date(birthyear);
-    var birthmonth = dateOfBirth.getMonth()+1;
-   
-//AQUARIUS
-    if (birthmonth == 1 && birthday >= 20 || birthmonth == 2 && birthday <= 18){
-      result = ("Aquarius");
-      document.getElementById("zodiacSignImg").src="images/aquarius.png";
-      document.getElementById("characteristic").innerHTML="Deep, imaginative, original & uncompromising.";
-    }
-//PISCES
-    if (birthmonth == 2 && birthday >= 19 || birthmonth == 3&& birthday <= 20){
-      result = ("Pisces");
-      document.getElementById("zodiacSignImg").src="images/pisces.png";
-      document.getElementById("characteristic").innerHTML="Affectionate, empathetic, wise & artistic.";
-    }
-//ARIES
-    if (birthmonth == 3 && birthday >= 21 || birthmonth == 4 && birthday <= 19){
-      result = ("Aries");
-      document.getElementById("zodiacSignImg").src="images/aries.png";
-      document.getElementById("characteristic").innerHTML="Eager, dynamic, quick & competitive.";
-    }
-//TAURUS
-    if (birthmonth == 4 && birthday >= 20|| birthmonth == 5 && birthday <= 20){
-      result = ("Taurus");
-      document.getElementById("zodiacSignImg").src="images/taurus.png";
-      document.getElementById("characteristic").innerHTML="Strong, dependable, sensual & creative.";
-    }
-//GEMINI
-    if (birthmonth == 5 && birthday >= 21 || birthmonth == 6 && birthday <= 20){
-      result = ("Gemini");
-      document.getElementById("zodiacSignImg").src="images/gemini.png";
-      document.getElementById("characteristic").innerHTML="Versatile, expressive, curious & kind.";
-    }
-//CANCER 
-    if (birthmonth == 6 && birthday >= 21 || birthmonth == 7&& birthday <= 22){
-      result = ("Cancer");
-      document.getElementById("zodiacSignImg").src="images/cancer.png";
-      document.getElementById("characteristic").innerHTML="Intuitive, sentimental, compassionate & protective.";      
-    }
-//LEO
-    if (birthmonth == 7 && birthday >= 23 || birthmonth == 8 && birthday <= 22){
-      result = ("Leo");
-      document.getElementById("zodiacSignImg").src="images/leo.png";
-      document.getElementById("characteristic").innerHTML="Dramatic, outgoing, fiery & self-assured.";      
-    }
-//VIRGO
-    if (birthmonth == 8 && birthday >= 23 || birthmonth == 9 && birthday <= 22){
-      result = ("Virgo");
-      document.getElementById("zodiacSignImg").src="images/virgo.png";
-      document.getElementById("characteristic").innerHTML="Practical, loyal, gentle & analytical.";
-    }
-//LIBRA  
-    if (birthmonth == 9 && birthday >= 23 || birthmonth == 10&& birthday <= 22){
-      result = ("Libra");
-      document.getElementById("zodiacSignImg").src="images/libra.png";
-      document.getElementById("characteristic").innerHTML="Social, fair-minded, diplomatic & gracious.";
-    }
-//SCORPIO
-    if (birthmonth == 10 && birthday >= 23|| birthmonth == 11 && birthday <= 21){
-      result = ("Scorpio");
-      document.getElementById("zodiacSignImg").src="images/scorpio.png";
-      document.getElementById("characteristic").innerHTML="Passionate, stubborn, resourceful & brave.";
-    }
-//SAGITTARIUS
-    if (birthmonth == 11 && birthday >= 22 || birthmonth == 12 && birthday <= 21){
-      result = ("Sagittarius");
-      document.getElementById("zodiacSignImg").src="images/sagittarius.png";
-      document.getElementById("characteristic").innerHTML="Extroverted, optimistic, funny & generous.";
-    }
-//CAPRICORN 
-    if(birthmonth == 12 && birthday >= 22 || birthmonth == 1 && birthday <= 19){ 
-      result = ("Capricorn");
-      document.getElementById("zodiacSignImg").src="images/capricorn.png";
-      document.getElementById("characteristic").innerHTML="Serious, independent, disciplined & tenacious.";
-    }
-//WRONG DATE
-    else if (birthmonth > 12 || birthday > 31){
-      alert("Please enter an accurate date ! ");
-    }
-//OUTPUT
-    document.getElementById("output").innerHTML= result;
-  }
 
-  
+//AQUARIUS
+if (year == "januari" && day >= "20" || year == "februari" && day <= 18){
+  result = ("Aquarius");
+  document.getElementById("zodiacSignImg").src="images/aquarius.png";
+  document.getElementById("characteristic").innerHTML="Deep, imaginative, original & uncompromising.";
+  }
+//PISCES
+if (year == "februari" && day >= 19 || year == "maart" && day <= 20){
+  result = ("Pisces");
+  document.getElementById("zodiacSignImg").src="images/pisces.png";
+  document.getElementById("characteristic").innerHTML="Affectionate, empathetic, wise & artistic.";
+}
+//ARIES
+if (year == "maart" && day >= 21 || year == "april" && day <= 19){
+  result = ("Aries");
+  document.getElementById("zodiacSignImg").src="images/aries.png";
+  document.getElementById("characteristic").innerHTML="Eager, dynamic, quick & competitive.";
+}
+//TAURUS
+if (year == "april" && day >= 20|| year == "mei" && day <= 20){
+  result = ("Taurus");
+  document.getElementById("zodiacSignImg").src="images/taurus.png";
+  document.getElementById("characteristic").innerHTML="Strong, dependable, sensual & creative.";
+}
+//GEMINI
+if (year == "mei" && day >= 21 || year == "juni" && day <= 20){
+  result = ("Gemini");
+  document.getElementById("zodiacSignImg").src="images/gemini.png";
+  document.getElementById("characteristic").innerHTML="Versatile, expressive, curious & kind.";
+}
+//CANCER 
+if (year == "juni" && day >= 21 || year == "juli" && day <= 22){
+  result = ("Cancer");
+  document.getElementById("zodiacSignImg").src="images/cancer.png";
+  document.getElementById("characteristic").innerHTML="Intuitive, sentimental, compassionate & protective.";      
+}
+//LEO
+if (year == "juli" && day >= 23 || year == "augustus" && day <= 22){
+  result = ("Leo");
+  document.getElementById("zodiacSignImg").src="images/leo.png";
+  document.getElementById("characteristic").innerHTML="Dramatic, outgoing, fiery & self-assured.";      
+}
+//VIRGO
+if (year == "augustus" && day >= 23 || year == "september" && day <= 22){
+  result = ("Virgo");
+  document.getElementById("zodiacSignImg").src="images/virgo.png";
+  document.getElementById("characteristic").innerHTML="Practical, loyal, gentle & analytical.";
+}
+//LIBRA  
+if (year == "september" && day >= 23 || year == "oktober" && day <= 22){
+  result = ("Libra");
+  document.getElementById("zodiacSignImg").src="images/libra.png";
+  document.getElementById("characteristic").innerHTML="Social, fair-minded, diplomatic & gracious.";
+}
+//SCORPIO
+if (year == "oktober" && day >= 23|| year == "november" && day <= 21){
+  result = ("Scorpio");
+  document.getElementById("zodiacSignImg").src="images/scorpio.png";
+  document.getElementById("characteristic").innerHTML="Passionate, stubborn, resourceful & brave.";
+}
+//SAGITTARIUS
+if (year == "november" && day >= 22 || year == "december" && day <= 21){
+  result = ("Sagittarius");
+  document.getElementById("zodiacSignImg").src="images/sagittarius.png";
+  document.getElementById("characteristic").innerHTML="Extroverted, optimistic, funny & generous.";
+}
+//CAPRICORN 
+if(year == "december" && day >= 22 || year == "januari" && day <= 19){ 
+  result = ("Capricorn");
+  document.getElementById("zodiacSignImg").src="images/capricorn.png";
+  document.getElementById("characteristic").innerHTML="Serious, independent, disciplined & tenacious.";
+}
+//WRONG DATE
+//else if (year > 12 || day > 31){
+  //alert("Please enter an accurate date ! ");
+//}
+//OUTPUT
+//document.getElementById("output").innerHTML= result;
