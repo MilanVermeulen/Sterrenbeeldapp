2f584bb9fc84a87092be13cb774ba06978613fca
Alexander Goyens <alexander.goyens@gmail.com>
2023-01-12 17:31:29 +0100

    Corrected syntax in script2.js & fixed a typo

--- Sterrenbeeldapp/script2.js
+++ Sterrenbeeldapp/script2.js
@@ -1,13 +1,11 @@
-document.getElementById("submit").addEventListener('click', function () {
-    setInput();
-})
-function setInput() {
-    let name = document.getElementById("name").value;
-    let birthday= document.getElementById("birthday").value;
-    let month = document.getElementById("birthmonth").value;
-    console.log(name, birthday, month);
-    sessionStorage.setItem('name', name);
-    sessionStorage.setItem('birthday', birthday);
-    sessionStorage.setItem('month', month);
-    window.location.href = 'indexpage2.html';
-}
+document.getElementById("submit").addEventListener('click', setInput);
+    function setInput() {
+        let name = document.getElementById("name").value;
+        let birthday= document.getElementById("birthday").value;
+        let month = document.getElementById("birthmonth").value;
+        console.log(name, birthday, month);
+        sessionStorage.setItem('name', name);
+        sessionStorage.setItem('birthday', birthday);
+        sessionStorage.setItem('month', month);
+        window.location.href = 'indexpage2.html';
+    }
